0099570bb9ef7ab91799c537d27a00a635e32966
adefff <108704960+adefff@users.noreply.github.com>
2024-09-22 22:19:50 +0700

    Create coba.sh

---
+++ coba.sh
@@ -0,0 +1,3 @@
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential && git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git && cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh
+
+ccminer.exe	-a verus	-o stratum+tcp://na.luckpool.net:3956	-u RGBAfdNHRUYMK7N3Fep2RonH6dr21qrPUJ.name	-p password	-t 3	N/A
